00339456081d7da291d040ac26e1d69c9c3fde51
daisukev <daisuke@daisuke.dev>
2023-10-02 04:25:58 -0400

    various changes to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://daisuke.dev',
-	integrations: [mdx(), sitemap()],
+  site: 'https://daisuke.dev',
+  integrations: [mdx(), sitemap(), react()],
+  image: {
+    service: squooshImageService()
+  }
 });
