f417956bebf9687bf37efa2b4342675ba25f7189
daisukev <daisuke@daisuke.dev>
2023-10-05 02:24:06 -0400

    modify description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Daisuke.dev";
-export const SITE_DESCRIPTION = "Personal site for Joshua Daisuke Vasquez.";
+export const SITE_DESCRIPTION =
+  "Joshua Daisuke Vasquez's web development portfolio";
